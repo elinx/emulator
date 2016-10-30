f71f685d20f475c8093df031f2e4dc726fdf45ad
tracer-ics2016 <tracer@njuics.org>
2016-10-30 22:50:14 +0800

    >  compile NEMU 151220000 xilinxing Linux xilinxing-Y470 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:01:55 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  22:50:14 up 1 day, 31 min,  1 user,  load average: 0.21, 0.15, 0.13 2cbc0e5eabc40f72be19a1b6d63f525d5f4351ee

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -82,6 +82,7 @@ int delete_wp(uint32_t N)
 				free_wp(w);
 				return 0;
 			} else { /* middle node to be deleted */
+				printf("prev: %d, p: %d", prev->NO, p->NO);
 				WP *w = p;
 				prev = p->next;
 				free_wp(w);

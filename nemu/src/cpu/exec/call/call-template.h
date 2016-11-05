cab8f80ff57ef37a1dcbc21a09550e8dad4c5942
tracer-ics2016 <tracer@njuics.org>
2016-11-05 22:52:04 +0800

    >  compile NEMU 151220000 xilinxing Linux xilinxing-Y470 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:01:55 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  22:52:04 up 14:13,  1 user,  load average: 0.41, 0.34, 0.35 d1d938f7fbd308b649bdf1088185a3c894f76038

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -9,7 +9,7 @@ static void do_execute()
 	MEM_W(REG(R_ESP), cpu.eip);
 
 	cpu.eip += op_src->val;                     // EIP += rel32/rel16
-	print_asm("call 0x%x", cpu.eip + DATA_BYTE + 1);
+	print_asm("call 0x%x", cpu.eip + DATA_BYTE + 1); // 1 for opcode
 }
 
 make_instr_helper(c)

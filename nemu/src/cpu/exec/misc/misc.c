3170e314710f94b94001929aed2f369bf0c98337
tracer-ics2016 <tracer@njuics.org>
2016-11-09 00:29:58 +0800

    >  compile NEMU 151220000 xilinxing Linux xilinxing-Y470 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:01:55 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  00:29:58 up 3 days, 15:51,  1 user,  load average: 0.23, 0.20, 0.26 b1d820dc2b69485253549fce804aa4018f838d1d

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -29,5 +29,5 @@ make_helper(ret) {
 	cpu.eip = swaddr_read(cpu.esp, 4);
 	cpu.esp -= 4;
 	print_asm("ret");
-	return 1; // sizeof(ret) == 1
+	return 0; // cos the otter will add eip by instruction length
 }

02a89c41a66660f630f5b4c53fec5c38273ff79a
tracer-ics2016 <tracer@njuics.org>
2016-11-12 00:09:17 +0800

    >  compile NEMU 151220000 xilinxing Linux xilinxing-Y470 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:01:55 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  00:09:17 up 6 days, 15:30,  1 user,  load average: 0.54, 0.41, 0.36 9cbe39beb8c865f2e33cb47e76ba11a324f496f

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -4,8 +4,9 @@
 
 static void do_execute () {
 	DATA_TYPE result = op_dest->val - op_src->val;
-	fprintf(stderr, "dest: %u src: %u", op_dest->val, op_src->val);
+	fprintf(stderr, "1 dest: %u src: %u", op_dest->val, op_src->val);
 	OPERAND_W(op_dest, result);
+	fprintf(stderr, "2 dest: %u src: %u", op_dest->val, op_src->val);
 
 	print_asm_template2();
 }
